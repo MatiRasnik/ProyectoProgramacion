66acefa0c4bf490f064123c7e79efb03287467a0
Juan Andrés Morena <thewolfmodzyt@gmaill.com>
2020-06-18 15:21:34 -0300

    RegistroPeliculas

---
+++ Proyecto Programacion/src/Almacen/RegistroPeliculas.java
@@ -0,0 +1,35 @@
+package Almacen;
+
+import java.util.Hashtable;
+
+public class RegistroPeliculas {
+
+	private Hashtable<String, Peliculas> RegistroPeliculas;
+	String encontrado;
+
+	public RegistroPeliculas() {
+		RegistroPeliculas = new Hashtable<String, Peliculas>();
+	}
+
+	public void agregarLibro(Peliculas peliculas) {
+		RegistroPeliculas.put(peliculas.getTitulo(), peliculas);
+	}
+
+	public String buscarTitulo(String titulo) {
+		Peliculas peliculas = RegistroPeliculas.get(titulo);
+
+		if (peliculas == null) {
+			System.out.println("Error");
+		} else {
+			encontrado = peliculas.getTitulo() + '\n' + peliculas.getDirector() + '\n' + peliculas.getProductora()
+					+ '\n' + peliculas.getGenero() + '\n' + peliculas.getFecha() + '\n' + peliculas.getDuracion() + '\n' + peliculas.getStock();
+		}
+		return encontrado;
+
+	}
+
+	public void eliminarLibro(String titulo) {
+		RegistroPeliculas.remove(titulo);
+	}
+
+}
