a87a99529d5bba30ff3df997f9cc27c7ded1c09b
MatiRasnik <MatiRasnik@users.noreply.github.com>
2020-06-18 14:55:02 -0300

    mas cosas

--- Proyecto Programacion/src/Almacen/RegistroODTeatro.java
+++ Proyecto Programacion/src/Almacen/RegistroODTeatro.java
@@ -1,5 +1,33 @@
 package Almacen;
+import java.util.Hashtable;
 
 public class RegistroODTeatro {
-
+	
+	private Hashtable<String, ObrasDeTeatro> RegistroODTeatro;
+	String encontrado;
+	
+	public RegistroODTeatro() {
+		RegistroODTeatro = new Hashtable<String, ObrasDeTeatro>();
+	}
+	
+	public void agregarLibro(ObrasDeTeatro odt) {
+		RegistroODTeatro.put(odt.getTitulo(), odt);
+	}
+	
+	public String buscarTitulo(String titulo) {
+		Libros libro = RegistroODTeatro.get(titulo);
+		
+		if(libro  == null) {
+			System.out.println("Error");
+		}else {
+			 encontrado = libro.getTitulo() + '\n' + libro.getAutor() + '\n' + libro.getTema() + '\n' + libro.getFecha() + '\n' + libro.getEditorial() + '\n' + libro.getPaginas();
+		}
+		return encontrado;
+	 
+	}
+	
+	public void eliminarLibro(String titulo) {
+		RegistroODTeatro.remove(titulo);
+	} 
+	
 }
