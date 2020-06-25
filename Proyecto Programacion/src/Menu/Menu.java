a1e15d68ef15e42a2e1086e2ea5c5b9b36cd76a9
Juan Andrés Morena <thewolfmodzyt@gmaill.com>
2020-06-25 12:26:57 -0300

    Menu

--- Proyecto Programacion/src/Menu/Menu.java
+++ Proyecto Programacion/src/Menu/Menu.java
@@ -1,12 +1,70 @@
 package Menu;
+import java.util.*;
+import Almacen.*;
 
 public class Menu {
-
+	
+	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		System.out.println("Hola");
-		System.out.println("Hola2");
+		int opcion = 0;
+		System.out.println("______________________");
+		System.out.println();
+		System.out.println("¿Que quiere almacenar?");
+		System.out.println("______________________");
+		System.out.println();
+		System.out.println("1. Libros");
+		System.out.println("2. Obras de Teatro");
+		System.out.println("3. Peliculas");
+		System.out.println("4. Pinturas");
+		System.out.println("______________________");
+		opcion = sc.nextInt();
+		
+		switch(opcion) {
+		case 1:
+			NuevoLibro();
+			break;
+		case 2:
+			
+			break;
+		case 3:
+			
+			break;
+		case 4:
+			
+			break;
+		}
 //menu
 
+	}
+	public static void NuevoLibro(){
+		String autor;
+		String tema;
+		String titulo;
+		String fecha;
+		String editorial;
+		int cantPaginas;
+		int stock;
+		
+		System.out.println("______________________");
+		System.out.println();
+		System.out.println("     Nuevo Libro:     ");
+		System.out.println("______________________");
+		sc = new Scanner(System.in);
+		System.out.print("Autor: ");
+		autor = sc.nextLine();
+		System.out.print("Tema: ");
+		tema = sc.nextLine();
+		System.out.print("Titulo: ");
+		titulo = sc.nextLine();
+		System.out.print("Fecha: ");
+		fecha = sc.nextLine();
+		System.out.print("Editorial: ");
+		editorial = sc.nextLine();
+		System.out.print("Cantidad de Paginas: ");
+		cantPaginas = sc.nextInt();
+		System.out.print("Stock: ");
+		stock = sc.nextInt();
+		
 	}
 
 }
