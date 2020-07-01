acc5391ec45c7dd0f3add327f05c220824215a68
Juan Andrés Morena <thewolfmodzyt@gmaill.com>
2020-07-01 15:32:59 -0300

    Menu

--- Proyecto Programacion/src/Menu/Menu.java
+++ Proyecto Programacion/src/Menu/Menu.java
@@ -54,6 +54,7 @@ public class Menu {
 		autor = sc.nextLine();
 		System.out.print("Tema: ");
 		tema = sc.nextLine();
+		
 		System.out.print("Titulo: ");
 		titulo = sc.nextLine();
 		System.out.print("Fecha: ");
