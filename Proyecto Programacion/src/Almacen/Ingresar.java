bade6a40b5dcd90cb5ff84f863cfad4ed1915923
MatiRasnik <MatiRasnik@users.noreply.github.com>
2020-07-01 18:01:00 -0300

    ingresar

--- Proyecto Programacion/src/Almacen/Ingresar.java
+++ Proyecto Programacion/src/Almacen/Ingresar.java
@@ -1,10 +1,12 @@
 package Almacen;
+
 import java.util.Scanner;
 
-public class Ingresar  {
+public class Ingresar {
 	static Scanner sc = new Scanner(System.in);
-	public static void Ingresar() {
-		
+
+	public Ingresar() {
+
 		int opcion = 0;
 		System.out.println("______________________");
 		System.out.println();
@@ -17,8 +19,8 @@ public class Ingresar  {
 		System.out.println("4. Pinturas");
 		System.out.println("______________________");
 		opcion = sc.nextInt();
-		
-		switch(opcion) {
+
+		switch (opcion) {
 		case 1:
 			NuevoLibro();
 			break;
@@ -32,134 +34,164 @@ public class Ingresar  {
 			NuevoPinturas();
 			break;
 		}
-//menu
 
 	}
-	public static void NuevoLibro(){
+
+	public static void NuevoLibro() {
+
 		RegistroLibros libro = new RegistroLibros();
-		String autor;
-		String tema;
-		String titulo;
-		String fecha;
-		String editorial;
-		int cantPaginas;
-		int stock;
-		
+		String autor = null;
+		String tema = null;
+		String titulo = null;
+		String fecha = null;
+		String editorial = null;
+		int cantPaginas = 0;
+		int stock = 0;
+		boolean error = false;
+
 		System.out.println("______________________");
 		System.out.println();
 		System.out.println("     Nuevo Libro:     ");
 		System.out.println("______________________");
-		sc = new Scanner(System.in);
-		System.out.print("Autor: ");
-		autor = sc.nextLine();
-		System.out.print("Tema: ");
-		tema = sc.nextLine();
-		System.out.print("Titulo: ");
-		titulo = sc.nextLine();
-		System.out.print("Fecha: ");
-		fecha = sc.nextLine();
-		System.out.print("Editorial: ");
-		editorial = sc.nextLine();
-		System.out.print("Cantidad de Paginas: ");
-		cantPaginas = sc.nextInt();
-		System.out.print("Stock: ");
-		stock = sc.nextInt();
-		
-		libro.agregarLibro(new Libros(autor, tema, titulo, fecha, editorial, cantPaginas, stock));
-		
+
+		try {
+			sc = new Scanner(System.in);
+			System.out.print("Autor: ");
+			autor = sc.nextLine();
+			System.out.print("Tema: ");
+			tema = sc.nextLine();
+			System.out.print("Titulo: ");
+			titulo = sc.nextLine();
+			System.out.print("Fecha: ");
+			fecha = sc.nextLine();
+			System.out.print("Editorial: ");
+			editorial = sc.nextLine();
+			System.out.print("Cantidad de Paginas: ");
+			cantPaginas = sc.nextInt();
+			System.out.print("Stock: ");
+			stock = sc.nextInt();
+		} catch (Exception ex) {
+			System.out.println("Debe ingresar todos los datos. ");
+			error = true;
+		}
+
+		if (error == false) {
+			libro.agregarLibro(new Libros(autor, tema, titulo, fecha, editorial, cantPaginas, stock));
+		}
 	}
-	
-	public static void NuevoODTeatro(){
+
+	public static void NuevoODTeatro() {
 		RegistroODTeatro odteatro = new RegistroODTeatro();
-		
-		String autor;
-		String titulo;
-		String genero;
-		String fecha;
-		int stock;
-		
-		System.out.println("________________________________");
+
+		String autor = null;
+		String titulo = null;
+		String genero = null;
+		String fecha = null;
+		int stock = 0;
+		boolean error = false;
+		try {
+			System.out.println("_______________________________");
+			System.out.println();
+			System.out.println("     Nueva Obra De Teatro:     ");
+			System.out.println("_______________________________");
+			sc = new Scanner(System.in);
+			System.out.print("Autor: ");
+			autor = sc.nextLine();
+			System.out.print("Titulo: ");
+			titulo = sc.nextLine();
+			System.out.print("Fecha: ");
+			fecha = sc.nextLine();
+			System.out.print("Genero: ");
+			genero = sc.nextLine();
+			System.out.print("Stock: ");
+			stock = sc.nextInt();
+		} catch (Exception ex) {
+			System.out.println("Debe ingresar todos los datos. ");
+			error = true;
+		}
+
+		if (error == false) {
+			odteatro.agregarODTeatro(new ObrasDeTeatro(autor, genero, titulo, fecha, stock));
+		}
+	}
+
+	
+
+	public static void NuevoPelicula() {
+		RegistroPeliculas peliculas = new RegistroPeliculas();
+		String director = null;
+		String genero = null;
+		String titulo = null;
+		String fecha = null;
+		String productora = null;
+		int duracion = 0;
+		int stock = 0;
+		boolean error = false;
+		try {
+		System.out.println("_________________________");
 		System.out.println();
-		System.out.println("     Nueva Obra De Teatro:     ");
-		System.out.println("________________________________");
+		System.out.println("     Nueva Pelicula:     ");
+		System.out.println("_________________________");
 		sc = new Scanner(System.in);
-		System.out.print("Autor: ");
-		autor = sc.nextLine();
-		System.out.print("Titulo: ");
-		titulo = sc.nextLine();
-		System.out.print("Fecha: ");
-		fecha = sc.nextLine();
+		System.out.print("Director: ");
+		director = sc.nextLine();
 		System.out.print("Genero: ");
 		genero = sc.nextLine();
-		System.out.print("Stock: ");
-		stock = sc.nextInt();
-		
-		odteatro.agregarODTeatro(new ObrasDeTeatro(autor, genero, titulo, fecha, stock));
-		
-	}
-	
-	public static void NuevoPelicula(){
-		String autor;
-		String tema;
-		String titulo;
-		String fecha;
-		String editorial;
-		int cantPaginas;
-		int stock;
-		
-		System.out.println("______________________");
-		System.out.println();
-		System.out.println("     Nuevo Libro:     ");
-		System.out.println("______________________");
-		sc = new Scanner(System.in);
-		System.out.print("Autor: ");
-		autor = sc.nextLine();
-		System.out.print("Tema: ");
-		tema = sc.nextLine();
 		System.out.print("Titulo: ");
 		titulo = sc.nextLine();
 		System.out.print("Fecha: ");
 		fecha = sc.nextLine();
-		System.out.print("Editorial: ");
-		editorial = sc.nextLine();
-		System.out.print("Cantidad de Paginas: ");
-		cantPaginas = sc.nextInt();
+		System.out.print("Productora: ");
+		productora = sc.nextLine();
+		System.out.print("Duracion: ");
+		duracion = sc.nextInt();
 		System.out.print("Stock: ");
 		stock = sc.nextInt();
 		
+	} catch (Exception ex) {
+		System.out.println("Debe ingresar todos los datos. ");
+		error = true;
 	}
-	
-	public static void NuevoPinturas(){
-		String autor;
-		String tema;
-		String titulo;
-		String fecha;
-		String editorial;
-		int cantPaginas;
-		int stock;
-		
-		System.out.println("______________________");
+
+	if (error == false) {
+		peliculas.agregarPelicula(new Peliculas(director, genero, titulo, fecha, productora, duracion, stock));
+	}
+
+	}
+
+	public static void NuevoPinturas() {
+		RegistroPinturas pintura = new RegistroPinturas();
+		String autor = null;
+		String titulo = null;
+		String fecha = null;
+		String museo = null;
+		int stock = 0;
+		boolean error = false;
+		try {
+		System.out.println("________________________");
 		System.out.println();
-		System.out.println("     Nuevo Libro:     ");
-		System.out.println("______________________");
+		System.out.println("     Nueva Pintura:     ");
+		System.out.println("________________________");
 		sc = new Scanner(System.in);
 		System.out.print("Autor: ");
 		autor = sc.nextLine();
-		System.out.print("Tema: ");
-		tema = sc.nextLine();
 		System.out.print("Titulo: ");
 		titulo = sc.nextLine();
 		System.out.print("Fecha: ");
 		fecha = sc.nextLine();
-		System.out.print("Editorial: ");
-		editorial = sc.nextLine();
+		System.out.print("Museo: ");
+		museo = sc.nextLine();
 		System.out.print("Cantidad de Paginas: ");
-		cantPaginas = sc.nextInt();
-		System.out.print("Stock: ");
 		stock = sc.nextInt();
-		
+	} catch (Exception ex) {
+		System.out.println("Debe ingresar todos los datos. ");
+		error = true;
+	}
+
+	if (error == false) {
+		pintura.agregarPinturas(new Pinturas(autor, titulo, fecha, museo, stock));
+	}
+
 	}
 
 }
-
-
